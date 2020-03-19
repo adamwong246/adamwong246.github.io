cbd919f3a4a55e0542d60119cf9553fde84b1c62
adam wong <adamwong246@gmail.com>
2020-03-19 12:04:30 -0700

    fix path

--- webcrack/index.js
+++ webcrack/index.js
@@ -1,17 +1,12 @@
 #!/usr/bin/env node
 
-http = require('http');
-sstatic = require('node-static');
-
 memoUniverse = require('./memo.js');
 build = require('./build.js');
 develop = require('./develop.js');
 server = require('./server.js');
 
-
-
 if (process.argv[2] == 'produce') {
-  return build(memoUniverse(''), {outFolder: './dist', minify: true});
+  return build(memoUniverse('https://raw.githubusercontent.com/adamwong246/adamwong246.github.io/master'), {outFolder: './dist', minify: true});
 } else if (process.argv[2] == 'develop') {
   develop(memoUniverse(''));
 }else if (process.argv[2] == 'preview') {
